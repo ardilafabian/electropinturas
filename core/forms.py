0696faf9e7eeeb6f955be65120e903220e77e214
Fabian Ardila <fabianra97@gmail.com>
2019-09-25 18:26:11 -0500

    Fix bug of mandatory country field

--- core/forms.py
+++ core/forms.py
@@ -10,16 +10,14 @@ PAYMENT_CHOICES = (
 class CheckoutForm(forms.Form):
     shipping_address = forms.CharField(required=False)
     shipping_address2 = forms.CharField(required=False)
-    shipping_country = CountryField(blank_label='(select country)').formfield(widget=CountrySelectWidget(attrs={
-        'required':False,
+    shipping_country = CountryField(blank_label='(select country)').formfield(required=False, widget=CountrySelectWidget(attrs={
         'class':'custom-select d-block w-100'
     }))
     shipping_zip = forms.CharField(required=False)
 
     billing_address = forms.CharField(required=False)
     billing_address2 = forms.CharField(required=False)
-    billing_country = CountryField(blank_label='(select country)').formfield(widget=CountrySelectWidget(attrs={
-        'required':False,
+    billing_country = CountryField(blank_label='(select country)').formfield(required=False, widget=CountrySelectWidget(attrs={
         'class':'custom-select d-block w-100'
     }))
     billing_zip = forms.CharField(required=False)
