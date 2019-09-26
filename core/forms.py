32f824ca4f3d4f32d7ac7b46c309c694552651d5
Fabian Ardila <fabianra97@gmail.com>
2019-09-25 20:29:08 -0500

    Translate to spanish checkout view

--- core/forms.py
+++ core/forms.py
@@ -10,14 +10,14 @@ PAYMENT_CHOICES = (
 class CheckoutForm(forms.Form):
     shipping_address = forms.CharField(required=False)
     shipping_address2 = forms.CharField(required=False)
-    shipping_country = CountryField(blank_label='(select country)').formfield(required=False, widget=CountrySelectWidget(attrs={
+    shipping_country = CountryField(blank_label='(Selecciona el país)').formfield(required=False, widget=CountrySelectWidget(attrs={
         'class':'custom-select d-block w-100'
     }))
     shipping_zip = forms.CharField(required=False)
 
     billing_address = forms.CharField(required=False)
     billing_address2 = forms.CharField(required=False)
-    billing_country = CountryField(blank_label='(select country)').formfield(required=False, widget=CountrySelectWidget(attrs={
+    billing_country = CountryField(blank_label='(Selecciona el país)').formfield(required=False, widget=CountrySelectWidget(attrs={
         'class':'custom-select d-block w-100'
     }))
     billing_zip = forms.CharField(required=False)
